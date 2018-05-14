44d6f7484b02427e301709a0632b81bb13609169
aleglez98 <38562163+aleglez98@users.noreply.github.com>
2018-05-14 15:42:57 +0100

    Update history.cpp

--- RedSocial1/src/history.cpp
+++ RedSocial1/src/history.cpp
@@ -1,2 +1,31 @@
 #include"../include/history.h"
 
+bool FIS::Register(string &user,string &passwd){
+   ifstream registro("basddatos.txt");
+   bool exito=false,encontrado=false;
+   i=1;
+   while(exito==false){
+      getline(registro,prueba);
+      if(prueba.find(usuario)!= -1){
+        if(prueba.find(",") != -1){
+          if(prueba.find(pass)!= -1){
+            cout << "Usuario correcto\n";
+            exito=true;
+            return exito;
+            }
+          }
+        }
+      if((exito==false)&&(i<=3)){
+        cout <<"Comprueba las credenciales"<<endl;
+        cout << "Introduzca su nombre de usuario:";
+        cin >> user;
+        cout << "Introduzca contraseña:";
+        cin >> passwd; 
+        }
+      if else((exito==false)&&(i>3)){
+        cout<<"no esta logeado por favor cree una cuenta"<<endl;
+        return exito;
+        }
+      i++;
+            }
+}
