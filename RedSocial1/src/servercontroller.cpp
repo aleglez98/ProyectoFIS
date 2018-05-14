b0dc05ce3836dc008c3037a6a80b8fec24ac2d32
aleglez98 <38562163+aleglez98@users.noreply.github.com>
2018-05-14 16:04:43 +0100

    Update servercontroller.cpp

--- RedSocial1/src/servercontroller.cpp
+++ RedSocial1/src/servercontroller.cpp
@@ -14,13 +14,18 @@ void ServerController::Register(void){
     InfoUser userI;
     sockaddr_in rem_address{}
     socket_.recieve_userInfo(socket_.get_fd(), userI, rem_address);
-    
+    bool reg=hist_.register(userI.name,userI.passwd);
+    if(reg==false){
+        std::cout << "fallo en la creacion del usuario\n";}
+    else{
+        std::cout << "El usuario se ha creado perfectamente\n";}
 }
 void ServerController::ControlLogin(void){
     InfoUser userI;
     sockaddr_in rem_address{}
     socket_.recieve_userInfo(socket_.get_fd(), userI, rem_address);
     hist_.register(userI.name,userI.passwd);
+    return;
 }
 
 void ServerController::serverSend(std::atomic<bool> &quit, const std::string &username){
